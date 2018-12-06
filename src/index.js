094760e9b400112f72f23245a2041ec1869ae050
caiosousa21 <caiofmsousa@gmail.com>
2018-12-06 13:47:37 -0200

    brincando com JSX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello, Caio</h1>, document.getElementById('root'));
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)} :))))))))))</h1>;
+    }
+    return <h1>Hello, Stranger >:(</h1>;
+}
+
+const user = {
+    firstName: 'Caio',
+    lastName: 'Sousa'
+};
+
+const element = getGreeting(user);
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
+
+ReactDOM.render(element, document.getElementById('root'));
 
