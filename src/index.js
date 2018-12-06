1b9b39d7f03d108e7ac72a1936a2c7177139dffe
caiosousa21 <caiofmsousa@gmail.com>
2018-12-06 15:57:12 -0200

    finalizando dia 06/12, iniciando implentações com tratamento de eventos

--- src/index.js
+++ src/index.js
@@ -1,40 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//classes componentes tem features como local state e métodos de ciclo de vida
-class Clock extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {date: new Date()}
+function Actionlink(){
+    function handleClick(e){
+        e.preventDefault();
+        console.log('link clickado');
     }
-
-    componentDidMount(){
-        this.timer = setInterval(
-                ()=>this.tick(),
-                1000
-            );
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.timer);
-    }
-
-    tick(){
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Olá novamente</h1>
-                <h2>Agora são{this.state.date.toLocaleTimeString()}</h2>
-            </div>
-        );
-    }
+    return(
+        <a href="#" onClick={handleClick}>
+            Clickme  
+        </a>
+    );
 }
+
 ReactDOM.render(
-    <Clock/>,
+    Actionlink(),
     document.getElementById('root')
 );
