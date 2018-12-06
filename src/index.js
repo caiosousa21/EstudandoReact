28a4404ec8f31c1784977d34457afc860c49f8b2
caiosousa21 <caiofmsousa@gmail.com>
2018-12-06 13:58:10 -0200

    vendo como react renderiza elementos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {formatName(user)} :))))))))))</h1>;
-    }
-    return <h1>Hello, Stranger >:(</h1>;
-}
-
-const user = {
-    firstName: 'Caio',
-    lastName: 'Sousa'
-};
-
-const element = getGreeting(user);
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
-
-
+function tick(){
+    const element = (
+        <div>
+            <h1>Olá Caio,</h1>
+            <h2>Agora são: {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );     
 ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
+
 
