0980c909c5bac1974c75a15fc17276043987cc5d
caiosousa21 <caiofmsousa@gmail.com>
2018-12-07 14:00:38 -0200

    componentes controlados

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class MeuForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = {valor: '', valor1:'Nome: '};
+        this.state = {valor: '', valor1:'Nome: ', sabor:'caio', texto:'Isso não faz nada.'};
         this.handleChange = this.handleChange;
         this.handleSubmit = this.handleSubmit;
     }
@@ -14,23 +14,42 @@ class MeuForm extends React.Component {
     }
 
     handleSubmit=(e)=>{
-        alert('Entrada: ' + this.state.valor);
+        alert('CAIO: ' + this.state.sabor);
         this.setState({valor1: this.state.valor});
+        this.setState({sabor: this.state.sabor});
         e.preventDefault();
     }
-    //JSX obrigatoriamente precisa de um event handler no onChange do form, se não o form é inmutável
+
+    mudaSabor=(e)=>{
+        this.setState({sabor: e.target.value});
+    }
+
+    
+    //JSX obrigatoriamente precisa de um event handler no onChange do elemento, se não o elemento é inmutável
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
                     {this.state.valor1}
+                    <br/>
                     <input type='text' value={this.state.valor} onChange={this.handleChange}/>
+                    <br/>
+                    <select value={this.state.sabor} onChange={this.mudaSabor}>
+                        <option value='neymar'>Ney</option>
+                        <option value='uau'>...</option>
+                        <option value='caio'>CAIO</option>
+                    </select>
+                    <br/>
+                    <textarea value={this.state.texto}/>
                 </label>
+                <br/>
                 <input type='submit' value='Submit'/>
             </form>    
         );
     }
 }
+
+
 
 ReactDOM.render(
     <MeuForm />,
