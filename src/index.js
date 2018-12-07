d9c70a4f65137475df5cfed9dbbdb83ae139c84c
caiosousa21 <caiofmsousa@gmail.com>
2018-12-07 10:31:22 -0200

    listas em react, e brincando mais com JSX

--- src/index.js
+++ src/index.js
@@ -1,76 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class LoginControl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleLoginClick = this.handleLoginClick;
-        this.handleLogoutClick = this.handleLogoutClick;
-        this.state = {isLoggedIn:false};
-    }
+//LISTA BASICA
+// function Listando(props) {
+//     const valores = props.valores;
+//     const lista = valores.map((valores, index) =>
+//         //react pede que todo item de um array ou iterator tenha uma chave única
+//         //usar index como chave pode gerar futuras tretas, o recomendado é que cada item do arrya tenha um id para ser usado como chave (key={valores.id})
+//         //a chave deve ficar no contexto da lista, não do item
+//         //a chava não é passada para o componente React, logo se precisar dela é preciso passa-lá de maneira explícita
+//         <li key={index}>{valores}</li>
+//     );
+//     return (
+//         <ul>{lista}</ul>
+//     );
+// }
+// const valores = [1, 2, 3, 4, 5];
 
-    handleLoginClick=()=>{
-        this.setState({isLoggedIn:true});
-    }
-
-    handleLogoutClick=()=>{
-        this.setState({isLoggedIn:false});
-    }
-
-    render(){
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-
-        if(isLoggedIn){
-            button = <LogoutButton onClick={this.handleLogoutClick}/>
-        }else{
-            button = <LoginButton onClick={this.handleLoginClick}/>
-        }
-
-        return(
-            <div>
-                <Receber isLoggedIn={isLoggedIn}/>
-                {button}
-            </div>
-        );
-    }
-}
-
-function LoginButton(props) {
+//DUAS LISTAS COM MESMAS KEYS
+//fazendo a função componente com todo seu código em JSX
+function Blog(props) {
     return (
-      <button onClick={props.onClick}>
-        Login
-      </button>
+        <div>
+            <ul>
+                {props.posts.map((post) =>
+                    <li key={post.id}>
+                        {post.title}
+                    </li>
+                )}
+            </ul>
+            <hr />
+            {props.posts.map((post) =>
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.content}</p>
+                </div>
+            )}
+        </div>
     );
-  }
-  
-  function LogoutButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Logout
-      </button>
-    );
-  }
-
-function BemVindoU(props) {
-    const caio = props.caio;
-    return <h1>Bem Vindo {caio}!</h1>;
 }
 
-function BemVindoV() {
-    return <h1>Registre-se</h1>;
-}
-function Receber(props) {
-    const isLoggedIn = props.isLoggedIn;
-    const caio = props.caio;
-    if (isLoggedIn) {
-        console.log(props);
-        return <BemVindoU caio={caio} />;
-    }
-    return <BemVindoV />;
-}
-
+const posts = [
+    { id: 1, title: 'Olá caio', content: 'CAIO' },
+    { id: 2, title: 'Neymar', content: 'uau' }
+];
 ReactDOM.render(
-    <LoginControl/>,
+    <Blog posts={posts} />,
     document.getElementById('root')
 );
