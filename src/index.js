ddee32e9b8482e1efcbe5cc34c684ccb49f8485e
caiosousa21 <caiofmsousa@gmail.com>
2018-12-07 08:51:58 -0200

    manuseando eventos em react

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Actionlink(){
-    function handleClick(e){
+function Actionlink() {
+    function handleClick(e) {
         e.preventDefault();
         console.log('link clickado');
     }
-    return(
+    return (
         <a href="#" onClick={handleClick}>
-            Clickme  
+            Clickme
         </a>
     );
 }
 
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+        this.handleClick = this.handleClick;
+    }
+    handleClick = () => {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+    render() {
+        return (
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'On' : 'Off'}
+                </button>
+        );
+    }
+}
+
 ReactDOM.render(
-    Actionlink(),
+    <Toggle/>,
     document.getElementById('root')
 );
