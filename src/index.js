b582c0c5d2a8b957fca8241c058595fd1aab0658
caiosousa21 <caiofmsousa@gmail.com>
2018-12-11 13:59:43 -0200

    exemplo do jogo da velha

--- src/index.js
+++ src/index.js
@@ -1,173 +1,149 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class ProductCategoryRow extends React.Component {
-    render() {
-      const category = this.props.category;
-      return (
-        <tr>
-          <th colSpan="2">
-            {category}
-          </th>
-        </tr>
-      );
-    }
+import './style.css';
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
-  class ProductRow extends React.Component {
-    render() {
-      const product = this.props.product;
-      const name = product.stocked ?
-        product.name :
-        <span style={{color: 'red'}}>
-          {product.name}
-        </span>;
-  
+  class Board extends React.Component {
+    renderSquare(i) {
       return (
-        <tr>
-          <td>{name}</td>
-          <td>{product.price}</td>
-        </tr>
+        <Square
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
       );
-    }
-  }
-  
-  class ProductTable extends React.Component {
-    render() {
-      const filterText = this.props.filterText;
-      const inStockOnly = this.props.inStockOnly;
-  
-      const rows = [];
-      let lastCategory = null;
-  
-      this.props.products.forEach((product) => {
-        if (product.name.indexOf(filterText) === -1) {
-          return;
-        }
-        if (inStockOnly && !product.stocked) {
-          return;
-        }
-        if (product.category !== lastCategory) {
-          rows.push(
-            <ProductCategoryRow
-              category={product.category}
-              key={product.category} />
-          );
-        }
-        rows.push(
-          <ProductRow
-            product={product}
-            key={product.name}
-          />
-        );
-        lastCategory = product.category;
-      });
-  
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      );
-    }
-  }
-  
-  class SearchBar extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleInStockChange = this.handleInStockChange.bind(this);
-    }
-    
-    handleFilterTextChange(e) {
-      this.props.onFilterTextChange(e.target.value);
-    }
-    
-    handleInStockChange(e) {
-      this.props.onInStockChange(e.target.checked);
-    }
-    
-    render() {
-      return (
-        <form>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={this.props.filterText}
-            onChange={this.handleFilterTextChange}
-          />
-          <p>
-            <input
-              type="checkbox"
-              checked={this.props.inStockOnly}
-              onChange={this.handleInStockChange}
-            />
-            {' '}
-            Only show products in stock
-          </p>
-        </form>
-      );
-    }
-  }
-  
-  class FilterableProductTable extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        filterText: '',
-        inStockOnly: false
-      };
-      
-      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleInStockChange = this.handleInStockChange.bind(this);
-    }
-  
-    handleFilterTextChange(filterText) {
-      this.setState({
-        filterText: filterText
-      });
-    }
-    
-    handleInStockChange(inStockOnly) {
-      this.setState({
-        inStockOnly: inStockOnly
-      })
     }
   
     render() {
       return (
         <div>
-          <SearchBar
-            filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}
-            onFilterTextChange={this.handleFilterTextChange}
-            onInStockChange={this.handleInStockChange}
-          />
-          <ProductTable
-            products={this.props.products}
-            filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}
-          />
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
         </div>
       );
     }
   }
   
+  class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [
+          {
+            squares: Array(9).fill(null)
+          }
+        ],
+        stepNumber: 0,
+        xIsNext: true
+      };
+    }
   
-  const PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-  ];
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      this.setState({
+        history: history.concat([
+          {
+            squares: squares
+          }
+        ]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
+      });
+    }
   
-  ReactDOM.render(
-    <FilterableProductTable products={PRODUCTS} />,
-    document.getElementById('container')
-  );
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      });
+    }
+  
+    render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+  
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button className='btnJump' onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+  
+      let status;
+      if (winner) {
+        alert('Vencedor: '+winner);
+      } else {
+        status = "Próx Jogador: " + (this.state.xIsNext ? "X" : "O");
+      }
+  
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={i => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(<Game />, document.getElementById("container"));
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
